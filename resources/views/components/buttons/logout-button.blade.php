19d37bd4070441d3d5b00ee8b376db4cf70c980e
Mapma3eka262 <71893244+Mapma3eka262@users.noreply.github.com>
2026-01-30 16:48:28 +0500

    Delete resources directory

--- resources/views/components/buttons/logout-button.blade.php
+++
@@ -1,6 +0,0 @@
-<form method="POST" action="{{ route('logout') }}">
-    @csrf
-    <button type="submit" class="btn btn-danger btn-sm" title="Выход">
-        <i class="fas fa-sign-out-alt"></i> Выход
-    </button>
-</form>
